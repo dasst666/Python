c962d5a49eda44414d8e1f7e27cba503cfbd28ce
unknown <you@example.com>
2021-11-22 00:11:23 +0600

    второй коммит

---
+++ test.py
@@ -0,0 +1,67 @@
+# friends = ["das", "tan", "dasst"]
+
+# # first_invite = friends[0]
+# # second_invite = friends[1]
+# # third_invite = friends[2]
+
+# # print(friends)
+# print(friends[0], friends[1], friends[2])
+
+# minus_guest = friends.pop(1)
+# print(f"minus guest is: {minus_guest}")
+
+# friends.insert(1, "mal")
+
+# print(friends[0], friends[1], friends[2])
+# # print(friends)
+
+# friends.insert(0,"first_new")
+# friends.insert(2, "new_middle")
+# friends.append("last_guest")
+
+# print(friends[0], friends[1], friends[2], friends[3], friends[4], friends[5])
+
+
+# print(friends.pop())
+# print(friends.pop())
+# print(friends.pop())
+# print(friends.pop())
+# print(friends[0], friends[1])
+# del friends[0]
+# del friends[0]
+# print(friends)
+# ****************************************************
+
+# countries = ["USA", "Kazakhstan", "Russia", "China", "Italy"]
+# print(countries)
+# print(f"This is sorted list of countries: {sorted(countries)}")
+# print(f"This is reverse sorted list of countries: {sorted(countries, reverse=True)}")
+# print(countries)
+# print(countries.reverse())
+# print(countries)
+# print(len(countries))
+# ****************************************************
+
+# pizzas = ["pepperoni", "margarita", "four seasons"]
+
+# for pizza in pizzas:
+#     print(f"I like {pizza} pizza")
+# print("I really like pizza !")
+
+# values = []
+# for value in range(1, 11):
+#     value = value**3
+#     values.append(value)
+# print(values)
+
+# print(values[0:3])
+# print(values[-3:])
+# print(values[3:-4])
+
+# friend_pizzas = pizzas[:]
+# pizzas.append("pizza")
+# friend_pizzas.append("another_pizza")
+# print(pizzas)
+# print(friend_pizzas)
+# ****************************************************
+
